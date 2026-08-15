e3f6e141733d7027e7b37fe52d9e7e7239dec56c
Leonardo <leonnardo.santtos1@gmail.com>
2026-08-15 00:12:15 -0300

    feat: adciona tratamento de erro generico

--- search.py
+++ search.py
@@ -28,56 +28,59 @@ while True:
         print("Busca vazia. Tente novamente.")
         continue
 
-    url_final = f"https://dummyjson.com/products/search?q={produto}"
+    url = f"https://dummyjson.com/products/search?q={produto}"
 
     print(f"Buscando por: {produto}....")
 
-    resposta = requests.get(url_final)
+    try:
+        resposta = requests.get(url)
 
-    if resposta.status_code == 200:
-        print("Sucesso! A API respondeu")
-        dados = resposta.json() #é um hashmap simplificado
+        if resposta.status_code == 200:
+            print("Sucesso! A API respondeu")
+            dados = resposta.json() #é um hashmap simplificado
 
-        print("\n---RESULTADO DA BUSCA---")
+            print("\n---RESULTADO DA BUSCA---")
 
-        if "products" in dados and len(dados["products"]) > 0:
-            item = dados["products"][0]
+            if "products" in dados and len(dados["products"]) > 0:
+                item = dados["products"][0]
 
-            titulo = item['title']
-            preco = item['price']
-            estoque = item['stock']
-            descricao = item.get('description', 'Produto exclusivo e sem descrição detalhada.')
-            avaliacao = item.get('rating', 'Ainda não avaliado.')
-            desconto = item.get('discountPercentage', 0)
+                titulo = item['title']
+                preco = item['price']
+                estoque = item['stock']
+                descricao = item.get('description', 'Produto exclusivo e sem descrição detalhada.')
+                avaliacao = item.get('rating', 'Ainda não avaliado.')
+                desconto = item.get('discountPercentage', 0)
 
-            print(f"Produto encontrado: {titulo}")
+                print(f"Produto encontrado: {titulo}")
 
-            if desconto > 0:
-                print(f"Preço: ${preco} (Desconto atual: {desconto}%)")
+                if desconto > 0:
+                    print(f"Preço: ${preco} (Desconto atual: {desconto}%)")
+                else:
+                    print(f"Preço: ${preco}")
+                
+                print(f"Estoque: {estoque} unidades | Avaliação: {avaliacao} estrelas")
+
+                prompt = (
+                    f"Você é um consultor de vendas honesto e objetivo, não precisa dizer que é, apenas aja como um consultor.Dê uma pequena saudação."
+                    f" Apresente o produto '{titulo}' cujo preço final é ${preco}. Se o desconto ({desconto}%) for maior que zero, mencione-o "
+                    f"como uma vantagem, mas NÃO tente calcular o preço original. "
+                    f"Use a descrição ({descricao}) para explicar a utilidade do produto. "
+                    f"Informe de maneira neutra que a avaliação média é de {avaliacao} estrelas e que "
+                    f"temos {estoque} unidades disponíveis. Não crie falsos sensos de urgência e não invente dados"
+                )
+
+                client = genai.Client(api_key=chave_secreta)
+
+                resposta_ia = client.models.generate_content(
+                    model='gemini-3.5-flash-lite',
+                    contents=prompt
+                )
+
+                print("\n---RESPOSTA DA IA ---")
+                print(resposta_ia.text)
             else:
-                print(f"Preço: ${preco}")
-            
-            print(f"Estoque: {estoque} unidades | Avaliação: {avaliacao} estrelas")
-
-            prompt = (
-                f"Você é um consultor de vendas honesto e objetivo, não precisa dizer que é, apenas aja como um consultor.Dê uma pequena saudação."
-                f" Apresente o produto '{titulo}' cujo preço final é ${preco}. Se o desconto ({desconto}%) for maior que zero, mencione-o "
-                f"como uma vantagem, mas NÃO tente calcular o preço original. "
-                f"Use a descrição ({descricao}) para explicar a utilidade do produto. "
-                f"Informe de maneira neutra que a avaliação média é de {avaliacao} estrelas e que "
-                f"temos {estoque} unidades disponíveis. Não crie falsos sensos de urgência e não invente dados"
-            )
-
-            client = genai.Client(api_key=chave_secreta)
-
-            resposta_ia = client.models.generate_content(
-                model='gemini-3.5-flash-lite',
-                contents=prompt
-            )
-
-            print("\n---RESPOSTA DA IA ---")
-            print(resposta_ia.text)
+                print("Nenhum produto encontrado com esse nome")
         else:
-            print("Nenhum produto encontrado com esse nome")
-    else:
-        print(f"Erro na busca. Código HTTP:{resposta.status_code}")
+            print(f"Erro na busca. Código HTTP:{resposta.status_code}")
+    except Exception as erro:
+        print(f"Tivemos um problema de conexão {erro}. Vamos tentar novamente!")
